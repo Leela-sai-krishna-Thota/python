d7871c65df02951bb1245e089baaff87e108ad3d
Leela-sai-krishna-Thota <166853985+Leela-sai-krishna-Thota@users.noreply.github.com>
2024-07-29 16:43:56 +0530

    Create armstrong number.py

---
+++ armstrong number.py
@@ -0,0 +1,17 @@
+#programe to check the armstrong number or not
+n=int(input("enter the number"))
+sum=0
+count=0
+t=n
+while t!=0:
+  t//=10
+  count =count+1
+t=n
+while n!=0:
+  r=n%10
+  sum=sum+r**count
+  n=n//10
+if t==sum:
+  print(t,"is armstrong number")
+else:
+  print(t,"is not a armstrong number")
