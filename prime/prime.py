1f4deb1d7e54202cf76f49ae572bbbb6eb149874
Leela-sai-krishna-Thota <166853985+Leela-sai-krishna-Thota@users.noreply.github.com>
2024-08-04 12:09:18 +0530

    Create prime.py

---
+++ prime/prime.py
@@ -0,0 +1,10 @@
+#program to check the whether the given number is prime or not
+n=int(input("enter the value"))
+i=2
+while i<n:
+  if n%i==0:
+    print("not prime")
+    break
+  i=i+1
+else:
+  print("prime")
