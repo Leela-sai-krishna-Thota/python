da4c983d0f724d9823f9504e60b9c84cb684126c
Leela-sai-krishna-Thota <166853985+Leela-sai-krishna-Thota@users.noreply.github.com>
2024-08-04 12:06:59 +0530

    Create patterns.py

---
+++ patterns.py
@@ -0,0 +1,21 @@
+#programe to print the * pattern
+i=5
+n=int(input("enter the n value"))
+while i>=0:
+  j=1
+  while j<=i:
+    print("*",end=" ")
+    j=j+1
+  print()
+  i=i-1
+
+#programe to print the * pattern
+i=1
+n=int(input("enter the n value"))
+while i<=n:
+  j=1
+  while j<=i:
+    print("*",end=" ")
+    j=j+1
+  print()
+  i=i+1
