216328617d8b8feb54dafa240ed04bacb5f12667
Leela-sai-krishna-Thota <166853985+Leela-sai-krishna-Thota@users.noreply.github.com>
2024-08-04 11:52:30 +0530

    Create arearec.py

---
+++ arearec.py
@@ -0,0 +1,7 @@
+#programe to find the area and perimeter of rectangle
+l=float(input('enter l value')) #length
+b=float(input('enter b value')) #breadth
+a=l*b#area
+p=2*(l+b) #perimeter
+print('area of a rectangle=',a)
+print('perimetre of a rectangle=',p)
