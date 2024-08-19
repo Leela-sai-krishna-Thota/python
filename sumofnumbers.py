916d699501639789966278a353a10915ffec316a
Leela-sai-krishna-Thota <166853985+Leela-sai-krishna-Thota@users.noreply.github.com>
2024-08-19 15:05:12 +0530

    Create sumofnumbers.py

---
+++ sumofnumbers.py
@@ -0,0 +1,15 @@
+#programe to print the sum of first 10  natural numbers using while loop
+i=1
+sum=0
+while i<=10:
+  sum=sum+i
+  i=i+1
+print(sum)
+
+
+
+#programe to print the sum of first 10 natural numbers using for loop
+sum=0
+for i in range(0,11):
+  sum=sum+i
+print(sum)
