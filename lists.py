0f28d4b9c7e34c9f129ddbaa350fb2abec778c59
Leela-sai-krishna-Thota <166853985+Leela-sai-krishna-Thota@users.noreply.github.com>
2024-09-23 16:21:05 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,58 @@
+#program for adding and average
+l=[10,20,30,40]
+print("sum=",sum(l))
+print("average=",sum(l)/len(l))
+
+#program for reversing the list
+l=[10,20,30,40]
+print(l[::-1])
+
+#program to reverse the list using reverse method
+l=[10,20,30,40]
+l.reverse()
+print(l)
+
+#program to extract even and odd numbers from a given list
+l=[]
+n=int(input("enter n value"))
+for i in range(n):
+  ele=int(input("enter the elements"))
+  l.append(ele)
+even=[]
+odd=[]
+for i in l:
+  if i%2==0:
+    even.append(i)
+  else :
+    odd.append(i)
+print("even list=",even)
+print("odd list=",odd)
+
+
+#program to to interchange first and last elements of a list
+l=[10,20,30,40]
+temp=l[0]
+l[0]=l[-1]
+l[-1]=temp
+print(l)
+
+#program to find the posisition of maximum and minimum elements in the list
+l=[]
+n=int(input("enter the n value"))
+for i in range(n):
+  ele=int(input("enter the elements"))
+  l.append(ele)
+print("maximum index",l.index(max(l)))
+print("minimum index",l.index(min(l)))
+
+
+#program to remove first occurence of a given elements in the  list
+l=[10,20,10,30]
+l.remove(10)
+print(l)
+
+#program to concatenate two lists
+l1=["m","na","i","abhi"]
+l2=["y","me","s","ram"]
+x=zip(l1,l2)
+print(list(x))
