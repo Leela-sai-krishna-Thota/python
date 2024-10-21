a649c1ed54b7eba645ddf6e0512efc4e23772fb7
Thota Leela Sai Krishna <166853985+Leela-sai-krishna-Thota@users.noreply.github.com>
2024-10-21 15:45:56 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,15 @@
+#program on functions
+def add(a,b):
+  print("Addition =",a+b)
+def subtract(a,b):
+  print("subtraction =",a-b)
+def multiplication(a,b):
+  print("multiplication =",a*b)
+def division(a,b):
+  print("division =",a/b)
+a=int(input("enetr the a value"))
+b=int(input("enter the b value"))
+add(a,b)
+subtract(a,b)
+multiplication(a,b)
+division(a,b)
