db63c6afcda93660dd02feaf7d1e140a2a8ebe23
Thota Leela Sai Krishna <166853985+Leela-sai-krishna-Thota@users.noreply.github.com>
2024-10-21 16:14:49 +0530

    Create function1.py

---
+++ function1.py
@@ -0,0 +1,7 @@
+#program to return values and multiple
+def multiplication(a,b):
+  return a*b
+a=int(input("enetr the a value"))
+b=int(input("enter the b value"))
+multiplication1=multiplication(a,b)
+print(multiplication1)
